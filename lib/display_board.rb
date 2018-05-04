ac2fc6a203500c6d450c91c6bec174df4076de36
srappaport <sarah.rappaport@greendot.org>
2018-05-04 17:15:43 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+board = [" "," "," "," "," "," "," "," "," "]
+def display_board(board)
+  row1 = " #{board[0]} | #{board[1]} | #{board[2]} "
+  row2 = "-----------"
+  row3 = " #{board[3]} | #{board[4]} | #{board[5]} "
+  row5 = " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+display_board(board)
