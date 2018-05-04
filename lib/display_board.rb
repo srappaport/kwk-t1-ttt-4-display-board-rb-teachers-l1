5e501806eec225d06012006c4fcaa523fd9d7462
srappaport <sarah.rappaport@greendot.org>
2018-05-04 17:16:18 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,5 +6,10 @@ def display_board(board)
   row2 = "-----------"
   row3 = " #{board[3]} | #{board[4]} | #{board[5]} "
   row5 = " #{board[6]} | #{board[7]} | #{board[8]} "
+  puts row1
+  puts row2
+  puts row3
+  puts row2
+  puts row5
 end
 display_board(board)
